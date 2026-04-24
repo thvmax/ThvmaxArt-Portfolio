d07e031271c7a2abae7d38c7eb4f8ee92dab885c
thvmax <thutasoe24@gmail.com>
2026-04-24 20:16:02 +0400

    Update: fixed 404 issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Portfolio from './components/Portfolio';
+import Portfolio from "./components/Portfolio";
 
-export default function Page() {
+export default function Home() {
   return <Portfolio />;
 }
